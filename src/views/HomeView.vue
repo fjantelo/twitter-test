cb683ba2a301f29a725dfcf248f0e1f93673b248
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-08-28 09:04:05 -0400

    Adds changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <a class="twitter-timeline" href="https://twitter.com/@BarackObama" data-width="300" data-height="400">
+      Tweets by TwitterDev
+    </a>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
